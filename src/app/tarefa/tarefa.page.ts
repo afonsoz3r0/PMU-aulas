d1c6fda2612750580a5131aef47ce98c3ac0ad3e
afons <afonsogmleite@gmail.com>
2025-11-25 14:56:42 +0000

    Initial commit: Projeto Ionic PMU Aulas - Fase 1

---
+++ src/app/tarefa/tarefa.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarefa',
+  templateUrl: './tarefa.page.html',
+  styleUrls: ['./tarefa.page.scss'],
+  standalone: false,
+})
+export class TarefaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
